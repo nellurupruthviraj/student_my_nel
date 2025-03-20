b0210a3e9e36d9a5e256f8bb848e5f0ce8c913f3
Nitin-K1221 <rknitin24@gmail.com>
2025-03-20 18:37:29 +0530

    Mar-19 practice

---
+++ nitin_code/Practice Page.py
@@ -0,0 +1,5 @@
+x,git y,z = ("Hello", "World","Hii")
+print(y)
+
+x,y,z = "Orange", "Banana", "Cherry"
+print(x)
--- rohit_code/interview.py
+++
@@ -1,8 +0,0 @@
-a = [1,2,3,5,"vcsavv","jbsbvh"]
-c =0
-for i in a:
-    b = type(i)
-    if b == str:
-        c = c+1
-print(c)
-l=[8,9,10,11]
