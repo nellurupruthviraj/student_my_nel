886f533a3b9a6c7ea0164db2f8b820bbf870966a
Sai Rohith <sairohithsept01@gmail.com>
2025-04-13 16:10:18 +0530

    json things

--- rohit_code/jsonimple.py
+++ rohit_code/jsonimple.py
@@ -3,8 +3,12 @@ from rohit_code.workingjson import *
 with open('student.json', 'r') as file:
     data = json.load(file)
 print("what do you want")
-b = input("enter 1 for how many males and females \n enter 2 to know avg maarks of subject ")
+b = input("enter 1 for how many males and females \n enter 2 to know avg maarks of subject \n enter 3 for student details")
 c = 0
+e = 0
 if b=="2":
     c = input("which subject do you want")
-ad(b,data,c)
+if b == "3":
+    e = input("enter the student name")
+ad(b,data,c,e)
+
